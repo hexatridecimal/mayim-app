c453e6a1838f5ce2b5a7e91c3e62f2212c5da9f7
Andrew Carroll <mrandrewcarroll@gmail.com>
2020-05-31 17:26:18 -0400

    Create LoginBloc

---
+++ lib/bloc/login_bloc.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  @override
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(
+    LoginEvent event,
+  ) async* {
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
+      try {
+        yield LoginInitial();
+      } catch (e) {
+        yield LoginFailure(error: e.toString());
+      }      
+    }
+  }
+}
